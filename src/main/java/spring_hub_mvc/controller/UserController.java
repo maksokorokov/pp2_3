dd80f2a659d75afbe631cea6481754477783ad42
Maxim Okorokov <okoro99@mail.ru>
2022-05-14 15:18:39 +0300

    Good

--- src/main/java/spring_hub_mvc/controller/UserController.java
+++ src/main/java/spring_hub_mvc/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import spring_hub_mvc.model.User;
 import spring_hub_mvc.service.UserService;
 
@@ -18,7 +20,37 @@ public class UserController {
     @GetMapping("/")
     public String showAllEmployee(Model model){
         List<User> allUsers = userService.getAllUsers();
-        model.addAttribute("user",allUsers);
+        model.addAttribute("allUser",allUsers);
         return "all-users";
     }
+
+    @GetMapping("create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("create")
+    public String createUser(User user){
+        userService.add(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("{id}")
+    public String updateUserForm(@PathVariable("id") Long id,Model model){
+        model.addAttribute("user",userService.get(id));
+        return "user-update";
+    }
+
+    @PostMapping("update")
+    public String updateUser(User user){
+        userService.update(user);
+        return "redirect:/";
+    }
+
 }
