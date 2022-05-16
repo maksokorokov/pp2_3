b3a14faa2b18fc2e94651eeaabf7ef897346c611
Maxim Okorokov <okoro99@mail.ru>
2022-05-16 11:23:25 +0300

    добавил init class

---
+++ src/main/java/spring_hub_mvc/service/InitClass.java
@@ -0,0 +1,20 @@
+package spring_hub_mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import spring_hub_mvc.model.User;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitClass {
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void initMethod(){
+        userService.add(new User("Ivan","Ivanov",22));
+        userService.add(new User("Максим","Максимов",26));
+        userService.add(new User("Petr","Petrov",5));
+    }
+}
